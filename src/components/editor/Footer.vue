28a028966315104091aedc63d6efc43d04298788
Nishida Yoko <yoko.nishida@upstairs.jp>
2021-07-18 23:37:13 +0900

    Update design

--- src/components/editor/Footer.vue
+++ src/components/editor/Footer.vue
@@ -1,17 +1,13 @@
 <template>
-  <v-chip-group
-    mandatory
-    active-class="primary--text"
-    center-active
-  >
-    <v-chip
+  <v-btn-toggle>
+    <v-btn
       v-for="(value,index) in functions"
       :key="`function-${index}`"
       @click="goTo(value.name)"
     >
       <v-icon>{{ value.icon }}</v-icon>
-    </v-chip>
-  </v-chip-group>
+    </v-btn>
+  </v-btn-toggle>
 </template>
 
 <script>
@@ -22,6 +18,7 @@ export default {
         {name: "trim", icon: "mdi-content-cut"},
         {name: "filter", icon: "mdi-image-filter-black-white"},
         {name: "text", icon: "mdi-format-text"},
+        {name: "drawing", icon: "mdi-lead-pencil"},
       ]
     }
   },
