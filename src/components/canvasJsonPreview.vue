ce6c7441b24a24223a4c146c16947f6cfac58485
yokom5 <yoko.nishida@upstairs.jp>
2021-07-26 01:01:31 +0900

    Add json-colorizer

--- src/components/canvasJsonPreview.vue
+++ src/components/canvasJsonPreview.vue
@@ -2,28 +2,52 @@
   <v-card
     v-if="!isEmpty"
     class="json-card"
+    dark
   >
     <v-card-title class="text-left">canvas.toJSON</v-card-title>
-    <v-card-text class="text-left">
-      {{ canvasJson }}
+    <v-card-text class="text-left json-text">
+      {{ colorizedJson }}
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-import { mapGetters}from "vuex";
+import {mapGetters} from "vuex";
+import colorize from "json-colorizer";
 
 export default {
+  data() {
+    return {
+      colorizedJson: {},
+      colorizeOptions: {
+        colors: {
+          STRING_KEY: 'green',
+          STRING_LITERAL: 'magenta.bold',
+          NUMBER_LITERAL: '#FF0000'
+        },
+        pretty: true
+      }
+    };
+  },
+  mounted() {
+    this.$set(this, "colorizedJson", colorize(this.canvasJson, this.colorizeOptions));
+    console.log(this.colorizedJson);
+  },
   computed: {
     ...mapGetters({
       canvasJson: "canvasJson"
     }),
-    isEmpty(){
-      console.log("canvasJson:", this.canvasJson);
+    isEmpty() {
       // eslint-disable-next-line no-prototype-builtins
       return !this.canvasJson || !this.canvasJson.hasOwnProperty("objects");
     }
   },
+  watch: {
+    canvasJson(newJson) {
+      this.$set(this, "colorizedJson", colorize(newJson, this.colorizeOptions));
+      console.log(this.colorizedJson);
+    }
+  }
 }
 </script>
 
@@ -31,5 +55,8 @@ export default {
 .json-card {
   width      : 100%;
   min-height : 100px;
+}
+.json-text {
+  color: chartreuse !important;
 }
 </style>
